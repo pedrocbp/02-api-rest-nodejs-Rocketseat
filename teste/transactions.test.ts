65660bac8ccf9c2153273d6acd0a449ac3f56a71
Pedro <pedromiguelboce@gmail.com>
2025-05-28 23:08:20 -0300

    feat: adiciona configuração de migrações no início dos testes de transações

--- teste/transactions.test.ts
+++ teste/transactions.test.ts
@@ -1,5 +1,6 @@
-import { it, beforeAll, afterAll, describe, expect } from 'vitest'
+import { it, beforeAll, afterAll, describe, expect, beforeEach } from 'vitest'
 import supertest from 'supertest'
+import { execSync } from 'node:child_process'
 import { app } from '../src/app'
 
 describe('Transactions routes', () => {
@@ -8,6 +9,11 @@ describe('Transactions routes', () => {
   })
   afterAll(async () => {
     await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
   })
 
   it('should be able to create a new transaction', async () => {
